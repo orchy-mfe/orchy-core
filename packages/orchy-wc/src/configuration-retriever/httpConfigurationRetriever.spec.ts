7669eb91f9d4cf1951adbe5ed87a4f8d9595b3e9
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2022-08-22 20:09:44 +0200

    config test

--- packages/orchy-wc/src/configuration-retriever/httpConfigurationRetriever.spec.ts
+++ packages/orchy-wc/src/configuration-retriever/httpConfigurationRetriever.spec.ts
@@ -1,7 +1,56 @@
+import chai, { expect, assert } from '@esm-bundle/chai'
+import chaiAsPromised from '@esm-bundle/chai-as-promised'
+import { Configuration } from '@orchy/models';
+
 import httpConfigurationRetriever from './httpConfigurationRetriever';
 
-describe('myFunction', () => {
-  it('adds two numbers together', () => {
-    assert([1.2].includes(2))
+chai.use(chaiAsPromised)
+
+const mockConfig: Configuration = {
+  "microFrontends": [
+    {
+      "entryPoints": [
+        "https://test.com"
+      ],
+      "id": "microfrontend-test-1",
+      "route": "/route/load",
+      "pageConfiguration": "page-config-initial",
+      "properties": {
+        "mfName": "Name test"
+      }
+    }
+  ],
+  "common": {
+    "stylesheets": [
+      "https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css"
+    ],
+    "importMap": {
+      "lodash": "https://unpkg.com/lodash@4.17.21/lodash.js"
+    }
+  }
+}
+
+describe('httpConfigurationRetriever', () => {
+
+  let initialFetch = globalThis.fetch
+  
+  before(() => {
+    // @ts-ignore
+    globalThis.fetch = (endpoint: string) => endpoint === '/api/v1/configuration/test-configuration.json'
+      ? Promise.resolve({ json: () => mockConfig }) : Promise.reject()
+  })
+
+  after(() => {
+    globalThis.fetch = initialFetch
+  })
+
+  it('correctly return wanted configuration', async () => {
+    const response = await httpConfigurationRetriever('test-configuration')
+
+    expect(response).to.deep.equal(mockConfig)
+  });
+
+  it('correctly reject for missing configuration', () => {
+    assert.isRejected(httpConfigurationRetriever('missing-configuration'))
   });
 });
---
+++ packages/orchy-wc/web-test-runner.config.js
@@ -0,0 +1,21 @@
+import { legacyPlugin } from '@web/dev-server-legacy';
+import { esbuildPlugin } from '@web/dev-server-esbuild';
+
+export default {
+  plugins: [
+    esbuildPlugin({ ts: true }),
+    legacyPlugin({
+      polyfills: {
+        webcomponents: true,
+        custom: [
+          {
+            name: 'lit-polyfill-support',
+            path: 'node_modules/lit/polyfill-support.js',
+            test: "!('attachShadow' in Element.prototype)",
+            module: false,
+          },
+        ],
+      },
+    }),
+  ],
+};
