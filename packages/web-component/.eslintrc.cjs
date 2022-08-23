e035878990790d71de9958a663ce438632182c1d
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2022-08-23 22:02:35 +0200

    Eslint

--- packages/web-component/.eslintrc.cjs
+++ packages/web-component/.eslintrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'unused-imports'],
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
@@ -11,5 +11,11 @@ module.exports = {
     '@typescript-eslint/camelcase': 0,
     '@typescript-eslint/no-var-requires': 0,
     'semi': ['error', 'never'],
+    'no-unused-vars': 'off',
+    "unused-imports/no-unused-imports": "error",
+		"unused-imports/no-unused-vars": [
+			"warn",
+			{ "vars": "all", "varsIgnorePattern": "^_", "args": "after-used", "argsIgnorePattern": "^_" }
+		]
   },
 }
