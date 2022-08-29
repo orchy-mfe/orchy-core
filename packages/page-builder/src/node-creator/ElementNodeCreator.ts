60bf72e14a9d66247ff5112dcc47bdbe4bcb6612
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2022-08-29 11:54:23 +0200

    Shim

--- packages/page-builder/src/node-creator/ElementNodeCreator.ts
+++ packages/page-builder/src/node-creator/ElementNodeCreator.ts
@@ -3,6 +3,14 @@ import {Subject} from "rxjs"
 
 import CommonNodeCreator from "./CommonNodeCreator"
 
+const ESM_SUFFIXES = [".esm.js", ".mjs"]
+
+declare global {
+    interface Window {
+        importShim?: (scriptUrl: string) => void;
+    }
+}
+
 class ElementNodeCreator extends CommonNodeCreator {
 
     currentNode: HTMLElement
@@ -19,9 +27,14 @@ class ElementNodeCreator extends CommonNodeCreator {
 
     private importScript() {
         if (this.elementConfiguration.url) {
-            const isEsmUrl = this.elementConfiguration.url.endsWith('.esm.js')
+            const scriptUrl = this.elementConfiguration.url
+            const isEsmUrl = ESM_SUFFIXES.some(suffix => scriptUrl.endsWith(suffix))
             if (isEsmUrl) {
-                import(this.elementConfiguration.url)
+                if(window.importShim) {
+                    window.importShim(scriptUrl)
+                } else {
+                    import(scriptUrl)
+                }
             } else {
                 const scriptCreator = new ElementNodeCreator({ attributes: { src: this.elementConfiguration.url }, type: 'element', tag: 'script' }, this.eventBus)
                 document.head.appendChild(scriptCreator.create())
