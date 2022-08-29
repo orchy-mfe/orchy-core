fbbb05ab4aa1fde35c1c736fbcbf4b95bdb1d6e2
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2022-08-29 18:27:19 +0200

    Script mapper

--- packages/web-component/src/importMap.ts
+++ packages/web-component/src/importMap.ts
@@ -1,19 +1,25 @@
-import {Configuration} from '@orchy/models'
+import { Configuration } from '@orchy/models'
 import { FrameworkConfiguration } from 'qiankun'
 import 'es-module-shims'
 
-const postProcessTemplate: FrameworkConfiguration["postProcessTemplate"] = (tplResult) => {
-    tplResult.scripts = tplResult.scripts.map(script => {
-        if(typeof script === 'string') {
-            script = script.replace('import(', 'importShim(')
-        }
-        return script
-    })
+type PostProcessTemplate = NonNullable<FrameworkConfiguration["postProcessTemplate"]>
+
+type SingleScript = ReturnType<PostProcessTemplate>["scripts"][0]
+
+const scriptMapper: (script: SingleScript) => SingleScript = (script: SingleScript) => {
+    if (typeof script === 'string') {
+        script = script.replace('import(', 'importShim(')
+    }
+    return script
+}
+
+const postProcessTemplate: PostProcessTemplate = (tplResult) => {
+    tplResult.scripts = tplResult.scripts.map(scriptMapper)
     return tplResult
 }
 
 const installImportMaps = (configurationContent: Configuration): Partial<FrameworkConfiguration> => {
-    if(configurationContent.common?.importMap) {
+    if (configurationContent.common?.importMap) {
         importShim.addImportMap(configurationContent.common.importMap)
 
         return {
