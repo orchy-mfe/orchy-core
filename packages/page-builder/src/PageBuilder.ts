d8c77e241e96fb1596f57e587784dbfe24654036
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2022-09-14 14:15:05 +0200

    fix: Layout node creator

--- packages/page-builder/src/PageBuilder.ts
+++ packages/page-builder/src/PageBuilder.ts
@@ -2,7 +2,7 @@ import {PageConfiguration} from '@orchy-mfe/models'
 import {Subject, ReplaySubject} from 'rxjs'
 
 import ElementNodeCreator from './node-creator/ElementNodeCreator'
-import ImpaginationNodeCreator from './node-creator/ImpaginationNodeCreator'
+import LayoutNodeCreator from './node-creator/LayoutNodeCreator'
 
 const createNode = (configuration: PageConfiguration, eventBus: Subject<unknown>): HTMLElement => {
     switch (configuration.type) {
@@ -10,7 +10,7 @@ const createNode = (configuration: PageConfiguration, eventBus: Subject<unknown>
             return new ElementNodeCreator(configuration, eventBus).create()
         case 'flex-column':
         case 'flex-row':
-            return new ImpaginationNodeCreator(configuration).create()
+            return new LayoutNodeCreator(configuration).create()
     }
 }
 
---
+++ packages/page-builder/src/node-creator/LayoutNodeCreator.ts
@@ -0,0 +1,22 @@
+import {LayoutPageConfiguration} from '@orchy-mfe/models'
+
+import CommonNodeCreator from './CommonNodeCreator'
+
+class LayoutNodeCreator extends CommonNodeCreator {
+
+    currentNode: HTMLElement
+
+    private static stylesByType: Record<LayoutPageConfiguration['type'], string> = {
+        'flex-column': 'display: flex; flex-direction: column',
+        'flex-row': 'display: flex; flex-direction: row'
+    }
+
+    constructor(configuration: LayoutPageConfiguration) {
+        super(configuration)
+        this.currentNode = document.createElement('div')
+        this.currentNode.setAttribute('style', LayoutNodeCreator.stylesByType[configuration.type])
+    }
+    
+}
+
+export default LayoutNodeCreator
