9cb06f123217e54b28cc98603b081bcc5854f71e
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2022-11-10 21:51:01 +0100

    Delete MicrofrontendProperties.ts

--- packages/models/src/models/properties/MicrofrontendProperties.ts
+++
@@ -1,7 +0,0 @@
-import {MicroPage} from '../configuration'
-
-type AdditionalProperties = {
-    baseUrl: string,
-}
-
-export type MicroFrontendProperties = MicroPage['properties'] & AdditionalProperties
