56d4a5832677f3d2b8f48ff903ccadc0de95d44c
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2022-11-16 00:06:00 +0100

    fix: mount process

--- packages/spa-adapter/src/spa-adapter.ts
+++ packages/spa-adapter/src/spa-adapter.ts
@@ -2,31 +2,17 @@ import {MicroFrontendProperties} from '@orchy-mfe/models'
 import {LitElement, html} from 'lit'
 import {property} from 'lit/decorators.js'
 
-export default abstract class OrchyMicroFrontend<E=unknown> extends LitElement {
+export default abstract class OrchyMicroFrontend<E = unknown> extends LitElement {
     abstract mount(orchyProperties?: MicroFrontendProperties<E>): Promise<void>
     abstract unmount(orchyProperties?: MicroFrontendProperties<E>): Promise<void>
 
-    private _orchyProperties?: MicroFrontendProperties<E>
-    private isOrchyBoot = false
-
     @property({attribute: false})
-    get orchyProperties (): MicroFrontendProperties<E> | undefined {
-        return this._orchyProperties
-    }
-
-    set orchyProperties (orchyProperties: MicroFrontendProperties<E> | undefined) {
-        this.isOrchyBoot = true
-        this.unmount(this._orchyProperties)
-        this._orchyProperties = orchyProperties
-        this.mount(orchyProperties)
-    }
+    private orchyProperties?: MicroFrontendProperties<E>
 
     protected override firstUpdated(changedProperties: Map<PropertyKey, unknown>): void {
         super.firstUpdated(changedProperties)
-        if(!this.isOrchyBoot) {
-            this.orchyProperties = undefined
-        }
-      }
+        this.mount(this.orchyProperties)
+    }
 
     public getContainer(): HTMLElement {
         return this.renderRoot as HTMLElement
--- packages/spa-adapter/types/spa-adapter.d.ts
+++ packages/spa-adapter/types/spa-adapter.d.ts
@@ -3,10 +3,7 @@ import { LitElement } from 'lit';
 export default abstract class OrchyMicroFrontend<E = unknown> extends LitElement {
     abstract mount(orchyProperties?: MicroFrontendProperties<E>): Promise<void>;
     abstract unmount(orchyProperties?: MicroFrontendProperties<E>): Promise<void>;
-    private _orchyProperties?;
-    private isOrchyBoot;
-    get orchyProperties(): MicroFrontendProperties<E> | undefined;
-    set orchyProperties(orchyProperties: MicroFrontendProperties<E> | undefined);
+    private orchyProperties?;
     protected firstUpdated(changedProperties: Map<PropertyKey, unknown>): void;
     getContainer(): HTMLElement;
     disconnectedCallback(): void;
