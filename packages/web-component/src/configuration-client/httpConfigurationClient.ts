5309651aa6ebec3b97c03d5d3b4ac395e9b11d22
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2022-11-25 22:21:45 +0100

    feat: removed spaces

--- packages/web-component/src/configuration-client/httpConfigurationClient.ts
+++ packages/web-component/src/configuration-client/httpConfigurationClient.ts
@@ -3,7 +3,6 @@ import ConfigurationClient from './configurationClient'
 const CONFIGURATION_API = '/api/v1/configuration'
 
 class HttpConfigurationClient implements ConfigurationClient {
-
     private abortController = new AbortController()
 
     public async retrieveConfiguration<T>(configurationName: string): Promise<T> {
--- packages/web-component/src/event-bus/EventBusSubject.ts
+++ packages/web-component/src/event-bus/EventBusSubject.ts
@@ -1,11 +1,9 @@
 import {ReplaySubject} from 'rxjs'
 
 export default class EventBusSubject<T> extends ReplaySubject<T> {
-
     public clearBuffer(): void {
         // eslint-disable-next-line @typescript-eslint/ban-ts-comment
         // @ts-ignore
         this._buffer = []
     }
-
 }
