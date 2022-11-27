3cb954cdab687a401c257575d92c78d557435a03
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2022-11-27 11:26:17 +0100

    feat: page builder integration

--- packages/page-builder/src/PageBuilder.ts
+++ packages/page-builder/src/PageBuilder.ts
@@ -2,21 +2,28 @@ import {MicroFrontendProperties, PageConfiguration} from '@orchy-mfe/models'
 import createNode from './node-creator'
 import enrichNode from './string-node'
 
+const stringConfigStrategy = (configuration: string, microFrontendProperties: MicroFrontendProperties) => {
+    const createdNode: ParentNode = document.createRange().createContextualFragment(configuration)
+    enrichNode(createdNode, microFrontendProperties)
+    return createdNode
+}
+
+const jsonConfigStrategy = (configuration: PageConfiguration, microFrontendProperties: MicroFrontendProperties) => {
+    const createdNode: ParentNode = createNode(configuration, microFrontendProperties)
+    pageBuilder(configuration.content || [], createdNode, microFrontendProperties)
+    return createdNode
+}
+
 export const pageBuilder = (
     configurations: Array<PageConfiguration | string>,
     root: ParentNode = document.createElement('div'),
     microFrontendProperties: MicroFrontendProperties
 ): ParentNode => {
     const childrens = configurations.map(configuration => {
-        let createdNode: ParentNode
         if(typeof configuration === 'string') {
-            createdNode = document.createRange().createContextualFragment(configuration)
-            enrichNode(createdNode, microFrontendProperties)
-        } else {
-            createdNode = createNode(configuration, microFrontendProperties)
-            pageBuilder(configuration.content || [], createdNode, microFrontendProperties)
+            return stringConfigStrategy(configuration, microFrontendProperties)
         }
-        return createdNode
+        return jsonConfigStrategy(configuration, microFrontendProperties)
     })
     root.replaceChildren(...childrens)
     return root
