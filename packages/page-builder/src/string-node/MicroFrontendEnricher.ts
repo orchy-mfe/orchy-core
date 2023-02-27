454e55899fef02d03c7ada8b9072ce5da1b90673
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2023-02-27 23:50:02 +0100

    fix: html attributes inject

--- packages/page-builder/src/string-node/MicroFrontendEnricher.ts
+++ packages/page-builder/src/string-node/MicroFrontendEnricher.ts
@@ -1,8 +1,15 @@
 import {MicroFrontendProperties} from '@orchy-mfe/models'
 
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-const propertiesSetter = (microFrontendProperties: MicroFrontendProperties) => (node: Node) => node.orchyProperties = microFrontendProperties
+const propertiesSetter = (microFrontendProperties: MicroFrontendProperties) => (element: Element) => {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const {'orchy-micro-frontend': _, ...attributes} = element.getAttributeNames().reduce((acc, name: string) => {
+        acc[name] = element.getAttribute(name)
+        return acc
+    }, {} as Record<string, string | null>)
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    element.orchyProperties = {...microFrontendProperties, ...attributes}
+}
 
 export const enrichMicroFrontendNode = (createdNode: DocumentFragment, microFrontendProperties: MicroFrontendProperties) => {
     const setProperties = propertiesSetter(microFrontendProperties)
