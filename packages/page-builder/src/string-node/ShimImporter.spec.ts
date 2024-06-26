989b60e4ddeaf83d4231a0bb0607d4f7cd56d323
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2024-06-26 22:23:36 +0200

    chore: upgrade happy dom

--- packages/page-builder/src/string-node/ShimImporter.spec.ts
+++ packages/page-builder/src/string-node/ShimImporter.spec.ts
@@ -13,10 +13,10 @@ describe('ShimImporter', () => {
     it('work if importShim defined', () => {
         window.importShim = vi.fn()
         const scriptElement = document.createRange().createContextualFragment('<script type="module" src="foo.jsx"></script>')
-        
+
         shimImporter(scriptElement)
 
-        expect(window.importShim).toHaveBeenCalledWith('foo.jsx')
+        expect(window.importShim).toHaveBeenCalledWith('http://localhost:3000/foo.jsx')
         expect(window.importShim).toHaveBeenCalledTimes(1)
         expect(scriptElement.firstElementChild).toBeNull()
     })
@@ -24,10 +24,10 @@ describe('ShimImporter', () => {
     it('works if script is a leaf', () => {
         window.importShim = vi.fn()
         const scriptElement = document.createRange().createContextualFragment('<div><script type="module" src="fuu.jsx"></script></div>')
-        
+
         shimImporter(scriptElement)
 
-        expect(window.importShim).toHaveBeenCalledWith('fuu.jsx')
+        expect(window.importShim).toHaveBeenCalledWith('http://localhost:3000/fuu.jsx')
         expect(window.importShim).toHaveBeenCalledTimes(1)
         expect(scriptElement.firstElementChild?.firstChild).toBeNull()
     })
